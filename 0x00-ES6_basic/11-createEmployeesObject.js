eedff9cc2f0dc6147719a3f1798790d656ac2fe8
Yasmineltair <yasmineltair293@gmail.com>
2024-05-28 15:01:48 +0000

    11

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,4 +1,3 @@
 export default function createEmployeesObject(departmentName, employees) {
-  const empolyee = { [departmentName]: empolyees };
-  return empolyee;
+  return { [departmentName]: [...employees] };
 }
