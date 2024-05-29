700ce5906ab0ce6bb6fe072f75e67dba6ccd8d35
Yasmineltair <yasmineltair293@gmail.com>
2024-05-29 21:01:46 +0000

    1

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,13 +1,13 @@
 export default function (boolean) {
-    return new promise((resolve, reject) => {
-        const object = {
-            status: 200,
-            body: 'success',
-        };
-        if (boolean == true) {
-            resolve(object);
-        } else {
-            reject(Error('The fake API is not working currently'))
-        }
-    });
+  return new promise((resolve, reject) => {
+    const object = {
+      status: 200,
+      body: 'success',
+    };
+    if (boolean === true) {
+      resolve(object);
+    } else {
+      reject(Error('The fake API is not working currently'));
+    }
+  });
 }
