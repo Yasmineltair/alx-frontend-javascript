44e6390986e45d022c9656024eb49b9172decae8
Yasmineltair <yasmineltair293@gmail.com>
2024-05-30 11:29:08 +0000

    0

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,3 +1,5 @@
-export default class ClassRoom(maxStudentsSize) {
-maxStudentsSize = _maxStudentsSize;
-};
+export default class ClassRoom {
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
