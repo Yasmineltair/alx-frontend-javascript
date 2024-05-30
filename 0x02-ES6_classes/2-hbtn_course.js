025a52fe52ebbf7845c98b0ae3e231345b2548a1
Yasmineltair <yasmineltair293@gmail.com>
2024-05-30 15:15:24 +0000

    2

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,12 +1,13 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
     if (typeof name !== 'string') {
-      throw new TypeError('Name must be string');
+      throw new TypeError('Name must be a string');
     } else if (typeof length !== 'number') {
       throw new TypeError('Length must be a number');
     } else if (!Array.isArray(students)) {
-      throw new TypeError('Students must be an array of strings')
+      throw new TypeError('Students must be an array of strings');
     }
+
     this._name = name;
     this._length = length;
     this._students = students;
@@ -26,22 +27,22 @@ export default class HolbertonCourse {
 
   set name(name) {
     if (typeof name !== 'string') {
-      throw new TypeError('Name must be string');
+      throw new TypeError('Name must be a string');
     }
     this._name = name;
   }
 
   set length(length) {
     if (typeof length !== 'number') {
-      throw new TypeError('Number must be number');
+      throw new TypeError('Length must be a number');
     }
     this._length = length;
   }
 
   set students(students) {
-    if (!Array.isArray(stdents)) {
+    if (!Array.isArray(students)) {
       throw new TypeError('Students must be an array of strings');
-    } 
+    }
     this._students = students;
   }
 }
